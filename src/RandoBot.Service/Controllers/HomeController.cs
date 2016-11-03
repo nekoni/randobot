64ef40055a92eade7e3e143e6b879e936144ac79
nekoni <mirko.gatto@gmail.com>
2016-11-03 18:08:31 +0200

    added privacy policy

--- src/RandoBot.Service/Controllers/HomeController.cs
+++ src/RandoBot.Service/Controllers/HomeController.cs
@@ -17,24 +17,11 @@ namespace RandoBot.Service
         }
 
         /// <summary>
-        /// The about endpoint.
-        /// </summary>
-        /// <returns>A view.</returns>
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        /// <summary>
         /// The contact endpoint.
         /// </summary>
         /// <returns>A view.</returns>
-        public IActionResult Contact()
+        public IActionResult PrivacyPolicy()
         {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
