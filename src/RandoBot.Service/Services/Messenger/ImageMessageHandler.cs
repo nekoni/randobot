f65d44f44ca9c01ceae42e833b6103a6ce396b01
nekoni <mirko.gatto@gmail.com>
2016-12-03 13:08:38 +0200

    get tag support

--- src/RandoBot.Service/Services/Messenger/ImageMessageHandler.cs
+++ src/RandoBot.Service/Services/Messenger/ImageMessageHandler.cs
@@ -1,6 +1,9 @@
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Messenger.Client.Objects;
+using MetadataExtractor;
+using MetadataExtractor.Formats.Exif;
 
 namespace RandoBot.Service.Services.Messenger
 {
@@ -42,10 +45,43 @@ namespace RandoBot.Service.Services.Messenger
             }
 
             await this.Processor.PictureRepository.InsertAsync(sender.Id, attachement.Payload.Url);
+
             var pictureUrl = await this.Processor.PictureRepository.GetRandomAsync(user.UserId);
+            var location = await this.GetLocationFromPictureAsync(pictureUrl);
+
+            if (location != null)
+            {
+                await this.SendTextAsync(sender, location);
+            }
+
             await this.SendPictureAsync(sender, pictureUrl);
 
             return true;
+        }
+
+        private async Task<string> GetLocationFromPictureAsync(string pictureUrl)
+        {
+            using (var client = new HttpClient())
+            {
+                using (var response = await client.GetAsync(pictureUrl))
+                {
+                    response.EnsureSuccessStatusCode();
+
+                    using (var inputStream = await response.Content.ReadAsStreamAsync())
+                    {
+                        var directories = ImageMetadataReader.ReadMetadata(inputStream);
+                        var gps = directories.OfType<GpsDirectory>().FirstOrDefault();
+        
+                        if (gps != null)
+                        {
+                            var location = gps.GetGeoLocation();
+                            return $"lat: {location.Latitude} long: {location.Longitude}";
+                        }
+                    }
+                }
+            }
+
+            return null;
         }
     }
 }
